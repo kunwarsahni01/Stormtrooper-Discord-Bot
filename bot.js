7f0602962e0a2c5c7503d2e663f24dcadb544f0d
Kunwar Sahni <THEBOSS@new-host-5.home>
2017-04-02 08:51:35 -0400

    added member leave message and began work on customization

--- bot.js
+++ bot.js
@@ -1,67 +1,76 @@
  const Discord = require('discord.js');
  const client = new Discord.Client();
  const newUsers = [];
- const config = require ("./config.json");
+ const config = require("./config.json");
 
-client.on('ready', () => {
-    console.log('Hello World!');
-    client.user.setGame('Type !help for help', 'Online');
-});
+ client.on('ready', () => {
+ 	console.log('Hello World!');
+ 	client.user.setGame('Type !help for help', 'Online');
+ });
 
-const prefix = "!";
+ const prefix = "!";
 
-const hook = new Discord.WebhookClient('295876945475010570', 'g-nt7KvObOOcc3Fhr1Vq4vciR2v6Jzz_FInZy0XBeJ_YkPNmst3c_3zEMJkn2X-jMU4T');
-hook.sendMessage('I am now alive!');
+ const hook = new Discord.WebhookClient('295876945475010570', 'g-nt7KvObOOcc3Fhr1Vq4vciR2v6Jzz_FInZy0XBeJ_YkPNmst3c_3zEMJkn2X-jMU4T');
+ hook.sendMessage('I am now alive!');
 
-client.on("guildMemberAdd", member => {
-  let guild = member.guild;
-  guild.defaultChannel.sendMessage("Welcome! " + member.user);
-});
+ client.on("guildMemberRemove", member => {
+ 	let guild = member.guild;
+ 	guild.defaultChannel.sendMessage(member.user + " Left!");
+ });
 
-client.on("guildCreate", guild =>{
-  console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user.username}`)
-});
+ client.on("guildMemberAdd", member => {
+ 	let guild = member.guild;
+ 	guild.defaultChannel.sendMessage("Welcome! " + member.user);
+ });
 
-client.on('message', message => {
-  if(message.author.bot)return;
-  if(!message.content.startsWith(config.prefix))return;
+ client.on("guildCreate", guild => {
+ 	console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user.username}`)
+ });
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(config.prefix.length);
+ client.on('message', message => {
+ 	if (message.author.bot) return;
+ 	if (!message.content.startsWith(config.prefix)) return;
+ 	let command = message.content.split(" ")[0];
+ 	command = command.slice(config.prefix.length);
 
-  let args = message.content.split(" ").slice(1);
+ 	let args = message.content.split(" ").slice(1);
 
-  if(command === "add"){
-    let numArray = args.map (n=> parseInt(n));
-    let total = numArray.reduce( (p,c) => p+c);
-    message.channel.sendMessage(total);
-  }
+ 	if (command === "customize") {
+ 		message.channel.sendMessage('Would you like to have a server welcome message? If so type yes');
+ 	}
 
-  if(command === "say"){
-    message.channel.sendMessage(args.join(" "));
-  }
 
-  if (command === "help"){
-    message.channel.sendMessage('I have sent you a list of commands in your Direct Messages');
-    message.author.sendMessage('```To see a list of commands visit: https://kunwarsahni01.github.io/Stormtrooper-Discord-Bot/  Version: Mark 0.3, BETA BUILD```');
-    message.author.sendMessage('```For further support please join: https://discord.gg/YucSE7t```')
-  }
+ 	if (command === "add") {
+ 		let numArray = args.map(n => parseInt(n));
+ 		let total = numArray.reduce((p, c) => p + c);
+ 		message.channel.sendMessage(total);
+ 	}
 
-  if (command === "hello"){
-	  message.channel.sendMessage('I will shoot you, Rebel Scum!, ' + message.author.username);
-	}
+ 	if (command === "say") {
+ 		message.channel.sendMessage(args.join(" "));
+ 	}
 
-  if(command === "ping"){
-    message.channel.sendMessage('pong');
-  }
+ 	if (command === "help") {
+ 		message.channel.sendMessage('I have sent you a list of commands in your Direct Messages');
+ 		message.author.sendMessage('```To see a list of commands visit:```https://kunwarsahni01.github.io/Stormtrooper-Discord-Bot/ ```Version: Mark 0.3, BETA BUILD```');
+ 		message.author.sendMessage('```For further support please join: https://discord.gg/YucSE7t```')
+ 	}
 
-  if(command === "avatar"){
-    message.channel.sendMessage(message.author.avatarURL);
-  }
+ 	if (command === "hello") {
+ 		message.channel.sendMessage('I will shoot you, Rebel Scum!, ' + message.author.username);
+ 	}
 
-  if(command === "webhook"){
-    message.channel.sendMessage(hook.sendMessage('Webhooks Work!'))
-  }
-});
+ 	if (command === "ping") {
+ 		message.channel.sendMessage('pong');
+ 	}
 
-client.login(config.token);
+ 	if (command === "avatar") {
+ 		message.channel.sendMessage(message.author.avatarURL);
+ 	}
+
+ 	if (command === "webhook") {
+ 		message.channel.sendMessage(hook.sendMessage('Webhooks Work!'))
+ 	}
+ });
+
+ client.login(config.token);
